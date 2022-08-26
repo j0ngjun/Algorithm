32fd2ba34e935e06a80b0a55918621805d390f73
j0ngjun <whdwns123v@gmail.com>
2022-08-26 19:33:19 +0900

    Leetcode 278. First Bad Version

---
+++ LeetCode/L20220826_278.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+public class L20220826_278 {
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+    /*public class Solution extends VersionControl {
+        // 이진 탐색 활용
+        public int firstBadVersion(int n) {
+            int start = 1;
+            int end = n;
+
+            while(start < end) {
+                int mid = start + (end - start) / 2;
+                if(isBadVersion(mid)) {
+                    end = mid;
+                }else {
+                    start = mid + 1;
+                }
+            }
+            
+            return start;
+        }
+    }*/
+}
